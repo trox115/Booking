084770a094c618fd0d623aebb0fa31d510ebb0db
trox115 <tofernandes67@gmail.com>
2020-04-24 23:04:01 +0100

    Routes and Pages working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage/LoginPage';
-
+import HomePage from './Pages/HomePage/HomePage';
+import LifestylePage from './Pages/LifestylePage/LifestylePage';
+import BarberPage from './Pages/BarberPage/BarberPage';
 import './App.css';
 
 function App() {
   return (
     <Switch>
-      
-
-      <Route exact path='/' component={LoginPage} />
-      <Route exact path="/" component={HomePage} />
+      <Route exact path="/" component={LoginPage} />
+      <Route exact path="/home" component={HomePage} />
+      <Route exact path="/lifestyle" component={LifestylePage} />
+      <Route exact path="/barber" component={BarberPage} />
     </Switch>
   );
 }
