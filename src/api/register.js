8c08e99f5c0f90943f091fffd54fcb1de895b43e
trox115 <tofernandes67@gmail.com>
2020-04-27 23:39:16 +0100

    registration working for localhost

--- src/api/register.js
+++ src/api/register.js
@@ -1,8 +1,26 @@
-import React from 'react';
 import axios from 'axios';
 
-function Register(...prop) {
-  axios.post('https://localhost:3001/registrations',);
+function Register({ ...props }) {
+  const { email, password, password_confirmation } = props;
+
+  axios
+    .post(
+      'http://localhost:3001/registrations',
+      {
+        user: {
+          email,
+          password,
+          password_confirmation,
+        },
+      },
+      true,
+    )
+    .then(response => {
+      console.log('registration response: ', response);
+    })
+    .catch(errors => {
+      console.log('registration error: ', errors);
+    });
 }
 
 export default Register;
