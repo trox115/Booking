8d56f8c7d60b6b804ab19b1e9a62024fbe7ce324
trox115 <tofernandes67@gmail.com>
2020-04-27 20:52:31 +0100

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import LoginPage from './Pages/LoginPage/LoginPage';
-import HomePage from './Pages/HomePage/HomePage';
-import LifestylePage from './Pages/LifestylePage/LifestylePage';
-import BarberPage from './Pages/BarberPage/BarberPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import HomePage from './components/HomePage/HomePage';
+import LifestylePage from './components/LifestylePage/LifestylePage';
+import BarberPage from './components/BarberPage/BarberPage';
 import NavBar from './common/navigation';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
